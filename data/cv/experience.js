87b8d067c10c24a9f4176bec340e4fe69ceb2217
mariia-bodnar <92818449+mariia-bodnar@users.noreply.github.com>
2022-06-02 13:33:16 +0300

    Create experience.js

---
+++ data/cv/experience.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    title: "WebSpirit",
+    content: `<p>
+      Creating langing, pages. Implemementing animations for them using css keyframes, jquery animation capabilities and gsap library.
+    </p>
+    <p>Plenty of DOM manipulation with jquery and js without libs</p>
+    <p>Connection to CRM systems using <s>PHP</s>.</p>`
+  }
+]
