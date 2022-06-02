98860ae04fbd82053fd541326580d3c26e31fc8e
mariia-bodnar <92818449+mariia-bodnar@users.noreply.github.com>
2022-06-02 14:05:53 +0300

    Update summary.js

--- data/cv/summary.js
+++ data/cv/summary.js
@@ -1,13 +1,15 @@
 export default `
 <p class="aboutMe">
   I'am self-taught developer. 
-  Studied with youtube videos, interactive app (Sololearn), w3school and udemy courses <a href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/">
-  (React - The Complete Guide (incl Hooks, React Router, Redux)</a> by Maximilian Schwarzmüller). 
+  Studied with youtube videos, interactive app (Sololearn), w3school and udemy courses 
+  <a href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/">
+  (React - The Complete Guide (incl Hooks, React Router, Redux)
+  </a> by Maximilian Schwarzmüller). 
 </p>
 
 <p class="aboutMe">
   I have been working as a frontend delevoper for six months. 
-  For the most part, I worked independently on projects, created websites and connected them the CRM system using php.
+  For the most part, I worked independently on projects, created websites and connected them to the CRM system using php.
   But I want study and work with more modern technologies.
   Previously, I studied at a medical university and worked as a doctor.
   I like self-development and learning something new.
