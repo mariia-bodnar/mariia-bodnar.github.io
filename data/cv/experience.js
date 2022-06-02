ad7a2b738ba99a095272737a7da21b0b2aea3a41
mariia-bodnar <92818449+mariia-bodnar@users.noreply.github.com>
2022-06-02 13:44:20 +0300

    Update experience.js

--- data/cv/experience.js
+++ data/cv/experience.js
@@ -5,6 +5,6 @@ export default [
       Creating landing, pages. Implemementing animations for them using css keyframes, jquery animation capabilities and gsap library.
     </p>
     <p>Plenty of DOM manipulation with jquery and js without libs</p>
-    <p>Connection to CRM systems using <s>PHP</s>.</p>`
+    <p>Connection to CRM systems using <strong>PHP</strong>.</p>`
   }
 ]
